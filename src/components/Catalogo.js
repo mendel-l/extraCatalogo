68ccac87982b493ba9a456d75c56dd848bb4af4a
Mendel <mendel@umes.edu.gt>
2023-11-14 11:53:21 -0600

    conexion con back-end

--- src/components/Catalogo.js
+++ src/components/Catalogo.js
@@ -1,32 +1,40 @@
-import React from 'react';
+// Catalogo.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Catalogo.css';
 
 const Catalogo = () => {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3009/api/productos');
+        setProductos(response.data);
+      } catch (error) {
+        console.error('Error al obtener productos:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="catalogo-container">
-      <h1>Mi Catálogo</h1>
-      <div className="producto">
-        <img src="url_de_la_imagen" alt="Producto" />
-        <p>Nombre del Producto</p>
-        <p>Precio: Q20</p>
-      </div>
-      <div className="producto">
-        <img src="url_de_la_imagen" alt="Producto" />
-        <p>Nombre del Producto</p>
-        <p>Precio: Q20</p>
-      </div>
-      <div className="producto">
-        <img src="url_de_la_imagen" alt="Producto" />
-        <p>Nombre del Producto</p>
-        <p>Precio: Q20</p>
-      </div>
-      <div className="producto">
-        <img src="url_de_la_imagen" alt="Producto" />
-        <p>Nombre del Producto</p>
-        <p>Precio: Q20</p>
+    <div>
+      <h1>Catálogo de Productos</h1>
+      <div>
+        {productos.map(producto => (
+          <div key={producto.IdInventario}>
+            <h3>{producto.Nombre}</h3>
+            <p>Cantidad Disponible: {producto.CantidadDisponible}</p>
+            <p>Precio de Venta: {producto.PrecioVenta}</p>
+            <p>Estado: {producto.Estado === 1 ? 'Activo' : 'Inactivo'}</p>
+            <hr />
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default Catalogo;
